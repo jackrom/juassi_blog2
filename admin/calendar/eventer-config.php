24c723b3174146a6e33e68c3e70afab17824c81b
Juan C. Reyes C <jackrom@live.com>
2014-01-14 03:37:26 -0430

    original version

--- admin/calendar/eventer-config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$server_link = mysql_connect("localhost", "jcarlos", "6662115Jc") or die(mysql_error());
-$db_link = mysql_select_db("jcarlos_blog") or die(mysql_error());
-?>
--- admin/calendar/session-check.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	
-	//Check if user is logged in
-	if( !isset( $user_array['username'])){
-		header("Location: index.php");
-	}
-?>
